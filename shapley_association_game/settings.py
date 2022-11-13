441ae9fcd29f1c6ec58adf35a80fbbacd55be3ef
tomishima <pumoco2904@icloud.com>
2022-11-13 19:44:37 +0900

    changed settings.py

--- shapley_association_game/settings.py
+++ shapley_association_game/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'shapley_association_game.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('MYSQL_DATABASE'),  # データベース名
+        'USER': env('MYSQL_USER'),
+        'PASSWORD': env('MYSQL_PASSWORD'),
+        'HOST': env('MYSQL_HOST'),
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
