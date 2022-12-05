da3032637e316ec56f1be2f009a9fa57e9f0ad9a
Ryo <pumoco2904@icloud.com>
2022-12-05 13:08:27 +0900

    csvからsqlファイルを作成するpyファイルを作成

---
+++ data/makesql.py
@@ -0,0 +1,21 @@
+import csv, shutil
+import sys
+
+filename = './data/keywords_fit2022.csv'
+base_filename = './data/base.sql'  # 元となるsqlファイル
+output_filename = './data/words_data.sql'  # DBに読み込ませるsqlファイル
+
+shutil.copyfile(base_filename, output_filename)  # baseをコピーして追記
+
+total_row = sum([1 for _ in open(filename)])
+
+with open(filename, encoding='utf-8-sig', newline='') as rf:
+    csvreader = csv.reader(rf)
+    with open(output_filename, 'a') as wf:
+        print("", file=wf)
+        for i, row in enumerate(csvreader):  # csvを1行ずつ読み込む
+            if not i == total_row - 1 :
+                print(f"{tuple(row[:-1])},", file=wf)
+            else:
+                print(f"{tuple(row[:-1])};", file=wf)
+print("Successfully Output")
