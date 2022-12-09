d7932679f25686afdc662a4f6815db75ace5db66
Ryo <pumoco2904@icloud.com>
2022-12-09 22:02:41 +0900

    削除したviewに伴い、不要なルーティングを削除

--- game/urls.py
+++ game/urls.py
@@ -6,8 +6,6 @@ from . import views
 app_name = 'game'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('title/', views.TitleView.as_view(), name="title"),
-    path('signup/', views.SignupView.as_view(), name="signup"),
     path('gaming/', views.GamingView.as_view(), name="gaming"),
     path('results/', views.ResultsView.as_view(), name="results"),
 ]
