e37ff8435eeadb251a2dcf911d093a68343a9c01
Ryo <pumoco2904@icloud.com>
2022-12-23 15:02:01 +0900

    変化があったチェックボックスのidを取得できるようにした

--- static/js/checkbox_queue.js
+++ static/js/checkbox_queue.js
@@ -1,4 +1,11 @@
-let a = 0
+let queue = [];
+const stim_dict = {
+    'stimulus-1': '1',
+    'stimulus-2': '2',
+    'stimulus-3': '3',
+    'stimulus-4': '4',
+    'stimulus-5': '5',
+}
 
 // inputタグのうち type が checkbox のもの全てを対象
 const checkboxes = document.querySelectorAll('input[type=checkbox]');
@@ -10,7 +17,13 @@ checkboxes.forEach((checkbox) => {
 
 // チェックボックスの状態が変更されたときの処理を定義する関数
 function handleCheckboxChange(event) {
-    console.log(a); // チェックされている場合はtrue、されていない場合はfalse
-    a += 1;
+
+    // 変化があった checkbox を取得
+    var changed_checkbox = this;
+    var changed_checkbox_id = changed_checkbox.id;
+    console.log(stim_dict[changed_checkbox_id]);
+
+    // console.log(queue)
     // 以下、必要な処理を記述する
 }
+
