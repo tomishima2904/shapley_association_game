02757bbb9f6223394f4b3f4fb0efd6a20043459d
tomishima <pumoco2904@icloud.com>
2022-12-29 14:46:14 +0900

    よりstatelessにし、getメソッドでの安全性を向上させた

--- game/views.py
+++ game/views.py
@@ -13,10 +13,13 @@ from typing import List, Tuple, Dict, Any, Union
 
 
 STIMULI_NUM = 5  # 刺激語の数
-QUESTIONS_NUM = 3  # 本当は87だがテスト用に
-STIMULI_HEADER = [f"stimulus_{i+1}" for i in range(STIMULI_NUM)]
-STIMULI_ORDER = [i for i in range(STIMULI_NUM)]
-RAMDOM_ORDER = False  # False ならばDBに載っている刺激語の順に提示
+QUESTIONS_NUM = 2  # 本当は 87 だが開発用に 2
+STIMULI_HEADER = [f"stimulus_{i+1}" for i in range(STIMULI_NUM)]  # DB中から刺激語を探す時に使う用のヘッダー
+STR_STIMULI_ORDER = [str(order+1) for order in range(STIMULI_NUM)]  # DBに保存する用の刺激語の提示順を表した文字列
+
+RAMDOM_ORDER = False  # False ならばDBに載っている刺激語の順に提示（現在は実装予定無し）
+
+# 質問のカテゴリに応じた質問文の辞書
 with open ("data/input_sentences.json", encoding='utf-8') as f:
     Q_SENTENCES = json.load(f)
 
@@ -31,20 +34,6 @@ class IndexView(generic.TemplateView):
             print("Hello!")
             request.session['status'] = 1  # ログイン画面に訪れたことが一度でもあれば status に 2^0(1) を加算
 
-        # ランダムに刺激語を提示する場合
-        if RAMDOM_ORDER:
-            stimuli_order = copy.copy(STIMULI_ORDER)
-            request.session['stimuli_order'] = random.shuffle(stimuli_order)
-            stimuli_header = copy.copy(STIMULI_HEADER)
-            request.session['stimuli_header'] = [stimuli_header[i] for i in range(stimuli_order)]
-
-        else:
-            request.session['stimuli_order'] = copy.copy(STIMULI_ORDER)
-            request.session['stimuli_header'] = copy.copy(STIMULI_HEADER)
-
-        str_stimuli_order = [str(order+1) for order in request.session['stimuli_order']]
-        request.session['q_order'] = ''.join(str_stimuli_order)
-
         return super().get(request, **kwargs)
 
 
@@ -57,17 +46,17 @@ class GamingView(generic.TemplateView):
     def get(self, request, *args, **kwargs):
 
         context = {}
-        left_questions = QUESTIONS_NUM  # ユーザーが答えなければいけない質問の残数
-        context['left_questions'] = left_questions
+        left_questions = QUESTIONS_NUM
+        context['left_questions'] = left_questions  # ユーザーが答えなければいけない質問の残数
+        qid = QUESTIONS_NUM - left_questions + 1  # ユーザーが答える質問のID
 
-        # 1問目
+        # 1問目の場合
         if request.session['status'] == 1:
 
             print("Game Start!")
             start_datetime = localtime(timezone.now())  # 開始の日付時刻を記憶
             request.session['status'] = 3  # ゲーム中なら status に 2^1(2) を加算
             request.session['session_id'] = start_datetime.strftime("%Y%m%d%H%M%S")  # 同ユーザーの異なるゲーム(セッション)を識別
-            qid = QUESTIONS_NUM - left_questions + 1  # ユーザーが答える質問のID
 
             # データベースに最初の質問IDを登録
             UserAnswers.objects.create(
@@ -75,19 +64,18 @@ class GamingView(generic.TemplateView):
                 datetime=start_datetime,
                 session_id=request.session['session_id'],
                 qid=qid,
-                q_order = request.session['q_order']
+                q_order = ''.join(STR_STIMULI_ORDER)  # ユーザーに提示する刺激語の順序
             )
 
         # context['stimuli']: Dict = {
-        #     header: list(Words.objects.filter(qid=qid).values_list(header, flat=True))[0] for header in request.session['stimuli_header']
+        #     header: list(Words.objects.filter(qid=qid).values_list(header, flat=True))[0] for header in STIMULI_HEADER
         # }  # qidに該当する刺激後を抽出する
         context['stimuli']: List = [
-            list(Words.objects.filter(qid=qid).values_list(header, flat=True))[0] for header in request.session['stimuli_header']
+            list(Words.objects.filter(qid=qid).values_list(header, flat=True))[0] for header in STIMULI_HEADER
         ]
-        q_sentence = Q_SENTENCES[Words.objects.filter(qid=qid).values('category')[0]['category']]
+        q_sentence = Q_SENTENCES[Words.objects.filter(qid=qid).values('category')[0]['category']]  # qidのカテゴリに応じた質問文を用意
         context['q_sentence'] = q_sentence[-1]  # ユーザーに提示する質問文
         print(context)
-        context['status_message'] = "スタート!"
 
         return render(request, self.template_name, context)
 
@@ -111,12 +99,12 @@ class GamingView(generic.TemplateView):
         results.save()  # 結果を保存
         left_questions = int(request.POST.get('left-questions'))
         left_questions -= 1
-        context['left_questions'] = left_questions
+        context['left_questions'] = left_questions  # ユーザーが答えなければいけない質問の残数
 
         # 質問がなくなった場合
         if left_questions == 0:
             print("ゲーム終了！")
-            # return redirect('/results/')
+            # return redirect('/results/')  # 本当はこのようにサーバー側で遷移させたかったがうまくいかんのでjsで制御
             request.session['status'] = 1  # ゲーム中ではないので 2^1(2) を減算
             return JsonResponse(context)
 
@@ -131,16 +119,16 @@ class GamingView(generic.TemplateView):
                 datetime=localtime(timezone.now()),
                 session_id=request.session['session_id'],
                 qid=qid,
-                q_order = request.session['q_order']
+                q_order = ''.join(STR_STIMULI_ORDER)  # ユーザーに提示する刺激語の順序
             )
 
             # context['stimuli']: Dict = {
-            #     header: list(Words.objects.filter(qid=qid).values_list(header, flat=True))[0] for header in request.session['stimuli_header']
+            #     header: list(Words.objects.filter(qid=qid).values_list(header, flat=True))[0] for header in STIMULI_HEADER
             # }  # qidに該当する刺激後を抽出する
             context['stimuli']: List = [
-                list(Words.objects.filter(qid=qid).values_list(header, flat=True))[0] for header in request.session['stimuli_header']
+                list(Words.objects.filter(qid=qid).values_list(header, flat=True))[0] for header in STIMULI_HEADER
             ]
-            q_sentence = Q_SENTENCES[Words.objects.filter(qid=qid).values('category')[0]['category']]
+            q_sentence = Q_SENTENCES[Words.objects.filter(qid=qid).values('category')[0]['category']]  # qidのカテゴリに応じた質問文を用意
             context['q_sentence'] = q_sentence[-1]  # ユーザーに提示する質問文
             print(context)
 
@@ -156,10 +144,3 @@ class ResultsView(generic.TemplateView):
         request.session['status'] = 1  # ゲーム中ではないので 2^1(2) を減算
 
         return super().get(request, **kwargs)
-
-
-    # def post(self, request, *args, **kwargs):
-
-    #     request.session['started'] = False
-
-    #     return super().get(request, **kwargs)
