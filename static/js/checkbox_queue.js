b413a03844d168446cc66e78ba7f9c991e13ff79
tomishima <pumoco2904@icloud.com>
2023-01-07 13:46:09 +0900

    インデントを整形。スペース2つが水晶らしい。

--- static/js/checkbox_queue.js
+++ static/js/checkbox_queue.js
@@ -1,34 +1,33 @@
 const stim_dict = {
-    'stimulus-1': '1',
-    'stimulus-2': '2',
-    'stimulus-3': '3',
-    'stimulus-4': '4',
-    'stimulus-5': '5',
+  "stimulus-1": "1",
+  "stimulus-2": "2",
+  "stimulus-3": "3",
+  "stimulus-4": "4",
+  "stimulus-5": "5",
 };
 
 // inputタグのうち type が checkbox のもの全てを対象
-const checkboxes = document.querySelectorAll('input[type=checkbox]');
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
 
 // 全ての checkbox に対して 'change' イベントを検知する
 checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('change', handleCheckboxChange);
+  checkbox.addEventListener("change", handleCheckboxChange);
 });
 
 // チェックボックスの状態が変更されたときの処理を定義する関数
 function handleCheckboxChange(event) {
+  // 変化があった checkbox を取得
+  var changed_checkbox = this;
+  var changed_checkbox_id = stim_dict[changed_checkbox.id];
 
-    // 変化があった checkbox を取得
-    var changed_checkbox = this;
-    var changed_checkbox_id = stim_dict[changed_checkbox.id];
-
-    // チェックが外された場合、リストから刺激語のidを削除
-    if (queue.includes(changed_checkbox_id) === true){
-        var index = queue.indexOf(changed_checkbox_id)
-        queue.splice(index, 1)
-    }
-    // チェックがされた場合、リストに刺激語のidを追加
-    else{
-        queue.push(changed_checkbox_id)
-    }
-    console.log(queue);
-};
+  // チェックが外された場合、リストから刺激語のidを削除
+  if (queue.includes(changed_checkbox_id) === true) {
+    var index = queue.indexOf(changed_checkbox_id);
+    queue.splice(index, 1);
+  }
+  // チェックがされた場合、リストに刺激語のidを追加
+  else {
+    queue.push(changed_checkbox_id);
+  }
+  console.log(queue);
+}
--- static/js/fetch.js
+++ static/js/fetch.js
@@ -19,18 +19,17 @@ answer_form.addEventListener("submit", (e) => {
 
   // 全ての刺激語がチェックされている場合は、フォームを送信する
   if (queue.length >= NUM_STIM) {
-
     const answer = document.getElementById("user-answer");
-		const checkboxes = document.getElementsByName('stimulus');
-		const checkbox_labels = document.getElementsByName('label-stimulus');
+    const checkboxes = document.getElementsByName("stimulus");
+    const checkbox_labels = document.getElementsByName("label-stimulus");
 
     // URLのクエリパラメータを管理
     const body = new URLSearchParams();
     body.append("user-answer", answer.value); // ユーザーの解答
     const u_order = queue.join(""); // こんな感じで刺激語の順序を文字列にしてdjango側に渡す
-		console.log("ユーザーが選択した刺激語の順序は "+u_order+" です");
+    console.log("ユーザーが選択した刺激語の順序は " + u_order + " です");
     body.append("u-order", u_order); // ユーザーが選択した刺激語の順序
-		body.append("left-questions", left_questions);
+    body.append("left-questions", left_questions);
 
     // fetch API の登場! リロードしなくても画面の一部を更新できるようになる
     fetch("", {
@@ -47,24 +46,24 @@ answer_form.addEventListener("submit", (e) => {
         // JSON形式に変換
         return response.json();
       })
-			// 残りの質問が0かどうかを判定し、0なら結果画面へ遷移
-			.then((response) => {
-				if (response.left_questions == 0) window.location.href = url_results;
-				return response;
-			})
-			// 0ではないので次の問題を用意
+      // 残りの質問が0かどうかを判定し、0なら結果画面へ遷移
+      .then((response) => {
+        if (response.left_questions == 0) window.location.href = url_results;
+        return response;
+      })
+      // 0ではないので次の問題を用意
       .then((response) => {
         // フォームをクリア
         answer.value = "";
         // 刺激語を更新 & チェクボックスをクリアな状態に
-				for (let i=0; i<NUM_STIM; i++){
-					checkbox_labels[i].innerText = response.stimuli[i];
-					checkboxes[i].checked = false;
-				}
-				// ユーザーが答えなければいけない質問数を更新する
-				left_questions = response.left_questions;
-				// ユーザーが選択した刺激語の順序を記憶する配列もクリアに
-				queue = [];
+        for (let i = 0; i < NUM_STIM; i++) {
+          checkbox_labels[i].innerText = response.stimuli[i];
+          checkboxes[i].checked = false;
+        }
+        // ユーザーが答えなければいけない質問数を更新する
+        left_questions = response.left_questions;
+        // ユーザーが選択した刺激語の順序を記憶する配列もクリアに
+        queue = [];
       })
       .catch((error) => {
         console.log(error);
