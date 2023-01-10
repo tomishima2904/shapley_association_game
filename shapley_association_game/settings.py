8bf4aa003deb01c5d9fb03a7054308b263202da3
tomishima <pumoco2904@icloud.com>
2023-01-10 20:58:35 +0900

    本番環境用の`settings.py`を準備

---
+++ shapley_association_game/settings.py
@@ -0,0 +1,62 @@
+from .settings_common import *
+
+
+# 本番運用環境用にセキュリティキーを生成し環境変数から読み込む
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+
+# 本番運用では必ずFalse
+DEBUG = False
+
+# 許可するホスト名のリスト
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+
+# 静的ファイルを配置する場所
+STATIC_ROOT = '/usr/share/nginx/html/static'
+MEDIA_ROOT = '/usr/share/nginx/html/media'
+
+# Amazon SES関連設定
+AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
+AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
+EMAIL_BACKEND = 'django_ses.SESBackend'
+
+# ロギング
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'game': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'formatter': 'prod',
+            'when': 'D', # ログローテーション（新しいファイルへの切り替え）間隔の単位（D=日）
+            'interval': 1, # ログローテンション間隔（1日単位）
+            'backupCount': 7, # 保存しておくログファイル数
+        },
+    },
+
+    'formatters': {
+        'prod': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s',
+            ])
+        },
+    }
+}
+
--- shapley_association_game/settings_common.py
+++ shapley_association_game/settings_common.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    'django_ses',
 ]
 
 MIDDLEWARE = [
