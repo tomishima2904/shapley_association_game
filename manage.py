86f26138dbf110d2a060a8dced08a542cd0d7bd5
tomishima <pumoco2904@icloud.com>
2023-01-10 20:02:51 +0900

    開発環境用の設定ファイルを読み込みように直した

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shapley_association_game.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shapley_association_game.settings_dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
