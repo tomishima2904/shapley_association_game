7740e748d184af875fb6e3fe2f4e984a8767262f
tomishima <pumoco2904@icloud.com>
2023-02-23 22:22:49 +0900

    環境変数の取得方法を変更

--- shapley_association_game/settings.py
+++ shapley_association_game/settings.py
@@ -21,22 +21,22 @@ INSTALLED_APPS = [
 ]
 
 # 本番運用環境用にセキュリティキーを生成し環境変数から読み込む
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # 本番運用では必ずFalse
 DEBUG = False
 
 # 許可するホスト名のリスト
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 
 # 静的ファイルを配置する場所
 STATIC_ROOT = '/usr/share/nginx/html/static'
 MEDIA_ROOT = '/usr/share/nginx/html/media'
 
 # Amazon SES関連設定
-AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
-AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
+AWS_SES_ACCESS_KEY_ID = os.environ['AWS_SES_ACCESS_KEY_ID']
+AWS_SES_SECRET_ACCESS_KEY = os.environ['AWS_SES_SECRET_ACCESS_KEY']
 EMAIL_BACKEND = 'django_ses.SESBackend'
 
 # ロギング
